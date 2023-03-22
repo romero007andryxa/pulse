0c000ae328bfe313a886527d3ef9d5be6194a89c
Andrii <mer.an.an2005@gmail.com>
2023-03-22 17:44:24 +0100

    tried to fix problems

--- src/js/script.js
+++ src/js/script.js
@@ -73,7 +73,7 @@ $(document).ready(function(){
         });
     });
 
-    /* function validateForms(form) {
+    function validateForms(form){
         $(form).validate({
             rules: {
                 name: {
@@ -85,55 +85,24 @@ $(document).ready(function(){
                     required: true,
                     email: true
                 }
-              },
+            },
             messages: {
                 name: {
                     required: "Пожалуйста, введите свое имя",
-                    minlength: jQuery.validator.format("Введите {0} символов!")
-                },
-                phone:  "Пожалуйста, введите свой номер телефона",
-                email: {
-                  required: "Пожалуйста, введите свою почту",
-                  email: "Неправильно введен адрес почты"
-                }
-              }
-        });
-    };
-
-    validateForms('#consultation-form')
-    validateForms('#consultation form')
-    validateForms('#order form') */
-
-    function validateForms(form){
-        $(form).validate({
-            rules: {
-                name: {
-                    required: true,
-                    minlength: 2
-                },
-                phone: "required",
-                email: {
-                  required: true,
-                  email: true
-                }
-              },
-            messages: {
-                name: {
-                    required: "Пожалуйста, введите свое имя",
-                    minlength: jQuery.validator.format("Введите {0} символов!")
-                },
+                    minlength: jQuery.validator.format("Введите {0} символа!")
+                  },
                 phone: "Пожалуйста, введите свой номер телефона",
                 email: {
                   required: "Пожалуйста, введите свою почту",
                   email: "Неправильно введен адрес почты"
                 }
-              }
+            }
         });
     };
 
-    validateForms('#consultation-form')
-    validateForms('#consultation form')
-    validateForms('#order form')
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
 
     $('input[name=phone]').mask("+7 (999) 999-99-99");
 
